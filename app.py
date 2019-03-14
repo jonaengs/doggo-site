745dbdb80104eaae6c7e87483b0f1a1f9d9bc0b0
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-14 16:36:59 +0100

    asd

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 
 from flask import Flask, send_file
+from DoggoFace.imagegen import generate_face
 
 app = Flask(__name__)
-from DoggoFace.imagegen import main
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 IMG_PATH = os.path.join(BASE_DIR, 'doggo_site/DoggoFace/Outputs/face.png')
 
+
 @app.route('/')
 def get_image():
-    main()
+    generate_face()
     return send_file(IMG_PATH, mimetype='image/png')
