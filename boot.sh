1f949f492651990262582bb767e86abb6f5844b3
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-15 17:47:12 +0100

    fix?

--- boot.sh
+++ boot.sh
@@ -2,4 +2,4 @@
 # this script is used to boot a Docker container
 source venv/bin/activate
 flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - app:app
